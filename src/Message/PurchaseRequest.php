burnbright/omnipay-poli

commit de0a7909d2d6bdc81f79b38a5bfbc9eae2106edd

Fix  PSR2 compatibility issues

src/Message/PurchaseRequest.php
@@ -70,7 +70,9 @@ class PurchaseRequest extends AbstractRequest
     {
         $postdata = $this->packageData($this->getData());
         $httpRequest = $this->httpClient->post(
-            $this->endpoint, array('Content-Type'=>'text/xml'), $postdata
+            $this->endpoint,
+            array('Content-Type'=>'text/xml'),
+            $postdata
         );
         $httpResponse = $httpRequest->send();
 
@@ -82,16 +84,19 @@ class PurchaseRequest extends AbstractRequest
         $authenticationcode = $data['AuthenticationCode'];
         unset($data['AuthenticationCode']);
         $fields = "";
-        foreach($data as $field => $value){
-            $fields .= str_repeat(" ",24)."<dco:$field>$value</dco:$field>\n";
+        foreach ($data as $field => $value) {
+            $fields .= str_repeat(" ", 24)."<dco:$field>$value</dco:$field>\n";
         }
+        $namespace = "http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.Contracts";
+        $i_namespace = "http://www.w3.org/2001/XMLSchema-instance";
+        $dco_namespace = "http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO";
+
         return '<?xml version="1.0" encoding="utf-8" ?>
-                <InitiateTransactionRequest xmlns="http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+                <InitiateTransactionRequest xmlns="" xmlns:i="'.$i_namespace.'">
                     <AuthenticationCode>'. $authenticationcode.'</AuthenticationCode>
-                    <Transaction xmlns:dco="http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO">'
+                    <Transaction xmlns:dco="'.$dco_namespace.'">'
                         .$fields.
                     '</Transaction>
                 </InitiateTransactionRequest>';
     }
-
 }

src/Gateway.php
@@ -49,5 +49,4 @@ class Gateway extends AbstractGateway
     {
         return $this->createRequest('\Omnipay\Poli\Message\CompletePurchaseRequest', $parameters);
     }
-
 }

src/Message/CompletePurchaseResponse.php
@@ -13,7 +13,7 @@ class CompletePurchaseResponse extends AbstractResponse
 {
 
     public function __construct(RequestInterface $request, $data)
-    {   
+    {
         $data = json_decode($data);
         if (!$data || !$data->TransactionRefNo) {
             throw new InvalidResponseException;

src/Message/PurchaseResponse.php
@@ -15,21 +15,25 @@ use Omnipay\Common\Message\RedirectResponseInterface;
 class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
 
-	protected $error;
+    protected $error;
 
-	public function __construct(RequestInterface $request, $data)
+    public function __construct(RequestInterface $request, $data)
     {
         $this->request = $request;
         $xml = new SimpleXMLElement($data);
         if (!$xml->TransactionStatusCode) {
             throw new InvalidResponseException;
         }
-        $errors = $xml->Errors->children('http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO');
+        $errors = $xml->Errors->children(
+            'http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO'
+        );
         $this->error = $errors[0]; //only store first error
-        $this->data = $xml->Transaction->children('http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO');
+        $this->data = $xml->Transaction->children(
+            'http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO'
+        );
     }
 
-	public function isSuccessful()
+    public function isSuccessful()
     {
         return false;
     }
@@ -70,5 +74,4 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
     {
         return null;
     }
-
 }

commit eb7859273859f3eda0cbf7f77e71beae9458185a

FIX: request was broken in de0a7909d2d6bdc81f79b38a5bfbc9eae2106edd (missing namespace)

src/Message/PurchaseRequest.php
@@ -92,7 +92,7 @@ class PurchaseRequest extends AbstractRequest
         $dco_namespace = "http://schemas.datacontract.org/2004/07/Centricom.POLi.Services.MerchantAPI.DCO";
 
         return '<?xml version="1.0" encoding="utf-8" ?>
-                <InitiateTransactionRequest xmlns="" xmlns:i="'.$i_namespace.'">
+                <InitiateTransactionRequest xmlns="'.$namespace.'" xmlns:i="'.$i_namespace.'">
                     <AuthenticationCode>'. $authenticationcode.'</AuthenticationCode>
                     <Transaction xmlns:dco="'.$dco_namespace.'">'
                         .$fields.